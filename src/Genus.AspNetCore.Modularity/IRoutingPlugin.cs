2f1ccf53922526e2a48cfc63e01f057ad2888323
genusP <genusp@outlook.com>
2018-01-16 13:31:11 +0300

    suppor adding MVC route from plugin

---
+++ src/Genus.AspNetCore.Modularity/IRoutingPlugin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genus.AspNetCore.Modularity
+{
+    public interface IRoutingPlugin
+    {
+        void MapRoute(IRouteBuilder builder, IServiceProvider services);
+    }
+}
---
+++ src/Genus.AspNetCore.Modularity/RouteBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Genus.Modularity;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Genus.AspNetCore.Modularity
+{
+    public static class RouteBuilderExtensions
+    {
+        public static IRouteBuilder MapPluginsRoutes(
+            this IRouteBuilder builder, 
+            IPluginManager pluginManager, 
+            IServiceProvider services, 
+            Func<IPlugin, bool> predicate = null)
+        {
+            foreach (var pluginDescr in pluginManager.LoadedPlugins)
+            {
+                if (pluginDescr.Plugin is IRoutingPlugin routerPlugin)
+                        if( predicate == null || predicate(pluginDescr.Plugin))
+                            routerPlugin.MapRoute(builder, services);
+            }
+            return builder;
+        }
+    }
+}
